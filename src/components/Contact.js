e02076578ce3d4c9ac18d2e3f809eff3c5ec0e2f
Felipe Morales <carrionfelipe15@gmail.com>
2023-02-07 00:17:05 -0500

    delete transition projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -68,83 +68,73 @@ export const Contact = () => {
           </Col>
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeIn" : ""
-                  }
-                >
-                  <h2>Contacto</h2>
-                  <form onSubmit={handleSubmit}>
-                    <Row>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="text"
-                          value={formDetails.firstName}
-                          placeholder="Primer Nombre"
-                          onChange={(e) =>
-                            onFormUpdate("firstName", e.target.value)
+              <div className={"animate__animated animate__fadeIn"}>
+                <h2>Contacto</h2>
+                <form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col size={12} sm={6} className="px-1">
+                      <input
+                        type="text"
+                        value={formDetails.firstName}
+                        placeholder="Primer Nombre"
+                        onChange={(e) =>
+                          onFormUpdate("firstName", e.target.value)
+                        }
+                      />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input
+                        type="text"
+                        value={formDetails.lasttName}
+                        placeholder="Ultimo Nombre"
+                        onChange={(e) =>
+                          onFormUpdate("lastName", e.target.value)
+                        }
+                      />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input
+                        type="email"
+                        value={formDetails.email}
+                        placeholder="Direccion De Correo"
+                        onChange={(e) => onFormUpdate("email", e.target.value)}
+                      />
+                    </Col>
+                    <Col size={12} sm={6} className="px-1">
+                      <input
+                        type="tel"
+                        value={formDetails.asunto}
+                        placeholder="Asunto"
+                        onChange={(e) => onFormUpdate("asunto", e.target.value)}
+                      />
+                    </Col>
+                    <Col size={12} className="px-1">
+                      <textarea
+                        rows="6"
+                        value={formDetails.message}
+                        placeholder="Asunto"
+                        onChange={(e) =>
+                          onFormUpdate("message", e.target.value)
+                        }
+                      ></textarea>
+                      <button type="submit">
+                        <span>{buttonText}</span>
+                      </button>
+                    </Col>
+                    {status.message && (
+                      <Col>
+                        <p
+                          className={
+                            status.success === false ? "danger" : "success"
                           }
-                        />
+                        >
+                          {status.message}
+                        </p>
                       </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="text"
-                          value={formDetails.lasttName}
-                          placeholder="Ultimo Nombre"
-                          onChange={(e) =>
-                            onFormUpdate("lastName", e.target.value)
-                          }
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="email"
-                          value={formDetails.email}
-                          placeholder="Direccion De Correo"
-                          onChange={(e) =>
-                            onFormUpdate("email", e.target.value)
-                          }
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="tel"
-                          value={formDetails.asunto}
-                          placeholder="Asunto"
-                          onChange={(e) =>
-                            onFormUpdate("asunto", e.target.value)
-                          }
-                        />
-                      </Col>
-                      <Col size={12} className="px-1">
-                        <textarea
-                          rows="6"
-                          value={formDetails.message}
-                          placeholder="Asunto"
-                          onChange={(e) =>
-                            onFormUpdate("message", e.target.value)
-                          }
-                        ></textarea>
-                        <button type="submit">
-                          <span>{buttonText}</span>
-                        </button>
-                      </Col>
-                      {status.message && (
-                        <Col>
-                          <p
-                            className={
-                              status.success === false ? "danger" : "success"
-                            }
-                          >
-                            {status.message}
-                          </p>
-                        </Col>
-                      )}
-                    </Row>
-                  </form>
-                </div>
-              )}
+                    )}
+                  </Row>
+                </form>
+              </div>
             </TrackVisibility>
           </Col>
         </Row>
